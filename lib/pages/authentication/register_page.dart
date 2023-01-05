269a49d4022d952de1a77c5ad3e82a96175dc0cd
Hp <oluwafisayomiogunyemi@gmail.com>
2023-01-05 18:13:07 +0100

    Creating a Startup Screen

---
+++ lib/pages/authentication/register_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+
+        ),
+      ),
+    );
+  }
+}
+
---
+++ lib/pages/startup_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StarupPage extends StatefulWidget {
+  const StarupPage({Key? key}) : super(key: key);
+
+  @override
+  State<StarupPage> createState() => _StarupPageState();
+}
+
+class _StarupPageState extends State<StarupPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
